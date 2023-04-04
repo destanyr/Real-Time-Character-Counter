de2432f7236ae1efd45d973de2764402769131fe
Destany R <119266807+destanyr@users.noreply.github.com>
2023-04-04 01:33:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const textareaEL = document.getElementById("textarea")
+const totalCounterEl = document.getElementById("total-counter")
+const remainingCounterEl = document.getElementById("remaining-counter")
+
+textareaEL.addEventListener("keyup", () => {
+    updateCounter()
+})
+
+updateCounter()
+
+function updateCounter() {
+    totalCounterEl.innerText = textareaEL.value.length
+    remainingCounterEl.innerText = textareaEL.getAttribute("maxLength") - textareaEL.value.length;
+}
